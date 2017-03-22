5be2b14e946fc4c960591314296436b672620e1c
ZHU ZICHEN <Frederick-Zhu@users.noreply.github.com>
2017-03-22 09:25:32 +0800

    添加了两个新函数getAllGrade和getTotalGpa
    getAllGrade可以获得全部成绩
    
    getTotalGpa可以获得总GPA
    
    以上两个函数和getGrade均可接受用户名密码或者session传入

--- portal_tools/_grade.py
+++ portal_tools/_grade.py
@@ -4,8 +4,13 @@ import bs4
 import portal_tools
 
 
-def getGrade(username, password, semester_id=123):
-    session = portal_tools.IdasSession(username, password)
+def getGrade(semester_id, **kwargs):
+    if kwargs.has_key('session'):
+        session = kwargs['session']
+    elif kwargs.has_key('username') and kwargs.has_key('password'):
+        session = portal_tools.IdasSession(kwargs['username'], kwargs['password'])
+    else:
+        raise ValueError
 
     get_grade = session.get('http://eams.uestc.edu.cn/eams/teach/grade/course/person!search.action',
                             params={'semesterId': semester_id, 'projectType': ''})
@@ -32,3 +37,52 @@ def getGrade(username, password, semester_id=123):
         grade_list.append(grade_info)
 
     return grade_list
+
+
+def getAllGrade(semester_id, **kwargs):
+    if kwargs.has_key('session'):
+        session = kwargs['session']
+    elif kwargs.has_key('username') and kwargs.has_key('password'):
+        session = portal_tools.IdasSession(kwargs['username'], kwargs['password'])
+    else:
+        raise ValueError
+
+    get_all_grade = session.post('http://eams.uestc.edu.cn/eams/teach/grade/course/person!historyCourseGrade.action',
+                                 params={'projectType': 'MAJOR'})
+
+    all_grade_table = bs4.BeautifulSoup(get_all_grade.content, 'html.parser').find_all(name='table')[1]
+
+    all_grade_list = list()
+
+    for tr in all_grade_table.find(name='tbody').find_all(name='tr'):
+        grade_info = dict()
+        td = tr.find_all(name='td')
+
+        grade_info['year'] = td[0].text.strip()
+        grade_info['code'] = td[1].text.strip()
+        grade_info['sn'] = td[2].text.strip()
+        grade_info['name'] = td[3].text.strip()
+        grade_info['type'] = td[4].text.strip()
+        grade_info['credit'] = td[5].text.strip()
+        grade_info['score'] = td[6].text.strip()
+        grade_info['total'] = td[7].text.strip()
+
+        all_grade_list.append(grade_info)
+
+    return all_grade_list
+
+
+def getTotalGpa(**kwargs):
+    if kwargs.has_key('session'):
+        session = kwargs['session']
+    elif kwargs.has_key('username') and kwargs.has_key('password'):
+        session = portal_tools.IdasSession(kwargs['username'], kwargs['password'])
+    else:
+        raise ValueError
+
+    get_all_grade = session.post('http://eams.uestc.edu.cn/eams/teach/grade/course/person!historyCourseGrade.action',
+                                 params={'projectType': 'MAJOR'})
+
+    grade_sum_table = bs4.BeautifulSoup(get_all_grade.content, 'html.parser').find_all(name='table')[0]
+
+    return grade_sum_table.find_all(name='tr')[-2].find_all(name='th')[-1].text.strip()
