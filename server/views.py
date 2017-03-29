7cc80985930adb808a74e499a0f107f4341ec1f4
ZHU ZICHEN <Frederick-Zhu@users.noreply.github.com>
2017-03-29 15:52:40 +0800

    修改为了在before_request中实例化PortalUtil

--- server/views.py
+++ server/views.py
@@ -1,10 +1,23 @@
 # -*- coding:utf-8 -*-
 import werkzeug.datastructures
-from flask import render_template, request, session, url_for, redirect, flash
+from flask import render_template, request, session, url_for, redirect, flash, g
 
 import portal_tools
 import portal_tools.errors
 from server import app
+
+
+@app.before_request
+def before_request():
+    if request.method == 'GET' and request.path != url_for('index_page'):
+        if session.has_key('username') and session.has_key('password'):
+            try:
+                g.portal = portal_tools.PortalUtil(username=session.get('username'),
+                                                   password=session.get('password'))  # type: portal_tools.PortalUtil
+            except:
+                return redirect(url_for(index_page))
+        else:
+            return redirect(url_for('index_page'))
 
 
 @app.route('/')
@@ -13,9 +26,6 @@ def index_page():
         return render_template('index.html', login_vaild=True)
     else:
         return render_template('index.html', login_vaild=False)
-
-
-
 
 
 @app.route('/login', methods=['POST', ])
@@ -55,13 +65,7 @@ def logout():
 
 @app.route('/final_exam_time')
 def final_exam_time():
-    if not session.has_key('username') or not session.has_key('password'):
-        return redirect(url_for('index_page'))
-
-    portal = portal_tools.PortalUtil(username=session.get('username'),
-                                     password=session.get('password'))
-
-    final_exam_time_list = portal.getFinalExamTime(semester_id=143)
+    final_exam_time_list = g.portal.getFinalExamTime(semester_id=143)
 
     final_exam_time_list.sort(key=lambda course: course['examBegin'])
 
