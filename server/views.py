023c82fd64be5b6c46a9c50b3a7719c35db9af34
ZHU ZICHEN <Frederick-Zhu@users.noreply.github.com>
2017-03-29 13:38:54 +0800

    首页WIP

--- server/views.py
+++ server/views.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
-
-from flask import render_template
+import werkzeug.datastructures
+from flask import render_template, request, session
 
 from server import app
 
@@ -8,3 +8,16 @@ from server import app
 @app.route('/')
 def index_page():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['POST', ])
+def login():
+    form_data = request.form  # type: werkzeug.datastructures.ImmutableMultiDict
+
+    if not form_data.has_key('username') and form_data.has_key('password'):
+        raise ValueError
+
+    session['username'] = form_data.get('username')
+    session['password'] = form_data.get('password')
+
+    return ''
